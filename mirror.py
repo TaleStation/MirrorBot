4d3c33bab15dc95a57930e7d0523f1803c13729a
Jolly <70232195+Jolly-66@users.noreply.github.com>
2023-04-28 13:59:07 -0400

    PR bodies will no longer ping people

--- mirror.py
+++ mirror.py
@@ -9,17 +9,17 @@ def clean_repo():
 	logger = logging.getLogger("log")
 	logger.debug("Cleaning local repo.")
 	subprocess.run(["git", "fetch", "--all"],
-	               stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+	               )
 	subprocess.run(["git", "checkout", "master"],
-	               stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+	               )
 	subprocess.run(["git", "reset", "--hard", "downstream/master"],
-	               stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+	               )
 	subprocess.run(["git", "clean", "-f"],
-	               stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+	               )
 	logger.debug("Deleting branches.")
 	for deletable_branch in [line.strip().decode() for line in subprocess.check_output(["git", "branch"]).splitlines() if line != b"* master"]:
 		subprocess.run(["git", "branch", "-D", deletable_branch],
-		               stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+		               )
 
 
 def mirror_pr(upstream, downstream, pr_id):
@@ -32,7 +32,7 @@ def mirror_pr(upstream, downstream, pr_id):
 		clean_repo()
 		logger.debug("Switching to mirror branch.")
 		subprocess.run(["git", "checkout", "-b", f"{config.mirror_branch_prefix}{pr_id}"],
-		               stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+		               )
 
 		try:
 			logger.debug("Cherry-picking merge commit.")
@@ -50,7 +50,7 @@ def mirror_pr(upstream, downstream, pr_id):
 			logger.info("PR was merged via squash.")
 			commits = original_pull.get_commits()
 			subprocess.run(["git", "fetch", "--all"],
-                            stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+                            )
 			if original_pull.merge_commit_sha in [c.sha for c in commits]:
 				for c in commits:
 					subprocess.run(["git", "cherry-pick", "--no-commit", "-n", c.sha])
@@ -67,14 +67,14 @@ def mirror_pr(upstream, downstream, pr_id):
 			subprocess.run(["git", "add", "-A", "."])
 
 		subprocess.run(["git", "commit", "--allow-empty", "--no-edit", "-m", original_pull.title],
-		               stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+		               )
 		logger.debug("Pushing to downstream.")
 		subprocess.run(["git", "push", "downstream",
-                  f"{config.mirror_branch_prefix}{pr_id}"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+                  f"{config.mirror_branch_prefix}{pr_id}"], )
 
 		logger.info("Creating pull request.")
 		result = downstream._Repository__create_pull(title=f"{config.mirror_pr_title_prefix}{original_pull.title}",
-                                               body=f"Original PR: {original_pull.html_url}\n-----\n{original_pull.body}",
+                                               body=f"Original PR: {original_pull.html_url}\n-----\n{original_pull.body.replace('@', '')}",
                                                base="master",
                                                head=f"{config.mirror_branch_prefix}{pr_id}",
                                                maintainer_can_modify=True)
@@ -100,14 +100,15 @@ def remirror_pr(upstream, downstream, mirror_pr_id):
 		clean_repo()
 		logger.debug("Switching to mirror branch.")
 		subprocess.run(["git", "checkout", "-b", f"{config.mirror_branch_prefix}{original_pull.number}"],
-		               stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+		               )
 		logger.debug("Cherry-picking merge commit.")
 		subprocess.run(["git", "cherry-pick", "-m", "1", original_pull.merge_commit_sha],
-		               stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+		               )
 		logger.debug("Force pushing to downstream.")
 		subprocess.run(["git", "push", "--force", "downstream",
-                  f"{config.mirror_branch_prefix}{original_pull.number}"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+                  f"{config.mirror_branch_prefix}{original_pull.number}"], )
 	except:
 		logger.exception("An error occured during remirroring.")
 	finally:
 		os.chdir(current_directory)
+        
